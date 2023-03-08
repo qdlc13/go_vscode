d8446ec71d6f7b5d98b266887f655588e802603f
fengqing <qdlc13@gmail.com>
2023-03-09 00:11:17 +0800

    2023.03.09.01

--- File/main.go
+++ File/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("wwww")
+	file, err := os.Open("E:/git_master/go_vscode/File/a.txt")
+	if err != nil {
+		fmt.Println("open err")
+	}
+
+	fmt.Printf("a.txt=%v", file)
+
+	err2 := file.Close()
+	if err2 != nil {
+		fmt.Println("close err")
+	}
+
 }
